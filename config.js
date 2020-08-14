c514c358ce2581aeb5aa8eac78ec926e36f1ca2b
Juan Cruz Cardozo <60722288+SylphidZoul@users.noreply.github.com>
2020-08-14 01:36:01 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
   api: {
     port: process.env.API_PORT || 3004,
-    dbUrl: process.env.DB_URL || 'mongodb+srv://db_user_sylphid:Olazabal3805@cluster0.gzgpp.mongodb.net/LoremDB?retryWrites=true&w=majority'
+    dbUrl: process.env.DB_URL || 
   },
   jwt: {
     secret: process.env.JWT_SECRET || 'secret'
@@ -11,11 +11,11 @@ module.exports = {
     optionsSuccessStatus: 200
   },
   google: {
-    clientId: process.env.GOOGLE_ID || '252898407705-nf1nqeck3dnbjrl7d7nf1evvhvmeubr8.apps.googleusercontent.com',
-    secret: process.env.GOOGLE_SECRET || 'tqKWwSeb6DAlIfR5VuLYBuM6'
+    clientId: process.env.GOOGLE_ID,
+    secret: process.env.GOOGLE_SECRET
   },
   facebook: {
-    clientId: process.env.FACEBOOK_ID || '1698932393605899',
-    secret: process.env.FACEBOOK_SECRET || '72c997923a9cccc5c170609d6ef7715d'
+    clientId: process.env.FACEBOOK_ID,
+    secret: process.env.FACEBOOK_SECRET'
   }
 }
